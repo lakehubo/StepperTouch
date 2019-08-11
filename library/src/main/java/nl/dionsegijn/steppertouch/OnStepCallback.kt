f6c51490e7e0ee3041f68219e29de8acfa7ccc79
lake <huboa1234@126.com>
2019-08-12 00:02:45 +0800

    双向门锁，单项门锁ui完成

--- library/src/main/java/nl/dionsegijn/steppertouch/OnStepCallback.kt
+++
@@ -1,8 +0,0 @@
-package nl.dionsegijn.steppertouch
-
-/**
- * Created by dionsegijn on 3/19/17.
- */
-interface OnStepCallback {
-    fun onStep(value: Int, positive: Boolean)
-}
--- library/src/main/java/nl/dionsegijn/steppertouch/StepperTouchView.java
+++ library/src/main/java/nl/dionsegijn/steppertouch/StepperTouchView.java
@@ -1,66 +1,52 @@
 package nl.dionsegijn.steppertouch;
 
-import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.Region;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewParent;
+import android.view.animation.Animation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
 import androidx.core.view.ViewCompat;
 import androidx.dynamicanimation.animation.DynamicAnimation;
 import androidx.dynamicanimation.animation.SpringAnimation;
 import androidx.dynamicanimation.animation.SpringForce;
 
+/**
+ * 双向门锁view
+ */
 public class StepperTouchView extends ConstraintLayout {
-
-    // Drawing properties
-    private RectF clippingBounds = new RectF();
-    private Path clipPath = new Path();
 
     // Animation properties
     private Float stiffness = 200f;
-    private Float damping = 0.6f;
+    private Float damping = 0.5f;
     private Float startX = 0f;
     private Float startY = 0f;
 
     // Indication if tapping positive and negative sides is allowed
     boolean sideTapEnabled = false;
     private boolean allowDragging = false;
-    private boolean isTapped = false;
 
-    // Style properties
-    private int stepperBackground = R.color.stepper_background;
-    private int stepperActionColor = R.color.stepper_actions;
-    private int stepperActionColorDisabled = R.color.stepper_actions_disabled;
-    private int stepperTextColor = R.color.stepper_text;
-    private int stepperButtonColor = R.color.stepper_button;
     private int stepperTextSize = 20;
     private boolean allowNegative = true;
     private boolean allowPositive = true;
     private boolean isVertical = false;
+    private boolean isSingle = false;//单项选择
 
     private FrameLayout viewCounter;
     private ConstraintLayout viewBackground;
-    private ImageView viewCounterIcon;
-    private ImageView viewNegative;
-    private ImageView viewPositive;
+    private ImageView viewCounterIcon;//按钮icon
+    private ImageView viewNegative;//上方img
+    private TextView textNegative;//上方文字
+    private ImageView viewPositive;//下方img
+    private TextView textPositive;//下方文字
     private FrameLayout viewBtnBg;//按钮背景颜色
     private int maxDistance = -1;
 
@@ -81,15 +67,8 @@ public class StepperTouchView extends ConstraintLayout {
     private void initAttrs(Context context, AttributeSet attrs) {
         TypedArray styles = context.getTheme().obtainStyledAttributes(attrs, R.styleable.StepperTouch, 0, 0);
         try {
+            isSingle = styles.getBoolean(R.styleable.StepperTouch_stepperSingle, false);
             isVertical = styles.getInt(R.styleable.StepperTouch_stepperOrientation, 0) == 1;
-            stepperBackground =
-                    styles.getResourceId(R.styleable.StepperTouch_stepperBackgroundColor, R.color.stepper_background);
-            stepperActionColor =
-                    styles.getResourceId(R.styleable.StepperTouch_stepperActionsColor, R.color.stepper_actions);
-            stepperActionColorDisabled = styles.getResourceId(R.styleable.StepperTouch_stepperActionsDisabledColor, R.color.stepper_actions_disabled);
-            stepperTextColor = styles.getResourceId(R.styleable.StepperTouch_stepperTextColor, R.color.stepper_text);
-            stepperButtonColor =
-                    styles.getResourceId(R.styleable.StepperTouch_stepperButtonColor, R.color.stepper_button);
             stepperTextSize = styles.getDimensionPixelSize(R.styleable.StepperTouch_stepperTextSize, stepperTextSize);
             allowNegative = styles.getBoolean(R.styleable.StepperTouch_stepperAllowNegative, true);
             allowPositive = styles.getBoolean(R.styleable.StepperTouch_stepperAllowPositive, true);
@@ -101,25 +80,32 @@ public class StepperTouchView extends ConstraintLayout {
     private void initView(Context context) {
         View view;
         if (isVertical) {
-            view = LayoutInflater.from(context).inflate(R.layout.stepper_touch_vertical, this, true);
+            if (isSingle) {
+                view = LayoutInflater.from(context).inflate(R.layout.stepper_touch_vertical_single, this, true);
+            } else {
+                view = LayoutInflater.from(context).inflate(R.layout.stepper_touch_vertical, this, true);
+            }
         } else {
             view = LayoutInflater.from(context).inflate(R.layout.stepper_touch, this, true);
         }
-        setClipChildren(true);
         viewCounter = view.findViewById(R.id.viewCounter);
         viewBackground = view.findViewById(R.id.viewBackground);
         viewCounterIcon = view.findViewById(R.id.viewCounterText);
         viewNegative = view.findViewById(R.id.textViewNegative);
-        viewPositive = view.findViewById(R.id.textViewPositive);
+        textNegative = view.findViewById(R.id.textNegative);
         viewBtnBg = view.findViewById(R.id.btn_icon_bg);
-
-        setWillNotDraw(false);
+        if (!isSingle) {
+            viewPositive = view.findViewById(R.id.textViewPositive);
+            textPositive = view.findViewById(R.id.textPositive);
+        }
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         maxDistance = h / 2 - w / 2;
+        if (isSingle)
+            maxDistance = h - w;
     }
 
     @Override
@@ -131,7 +117,6 @@ public class StepperTouchView extends ConstraintLayout {
                     startY = event.getY();
                     allowDragging = true;
                 } else if (sideTapEnabled) {
-                    isTapped = true;
                     viewCounter.setX(event.getX() - viewCounter.getWidth() * 0.5f);
                 }
                 getParent().requestDisallowInterceptTouchEvent(true);
@@ -140,8 +125,6 @@ public class StepperTouchView extends ConstraintLayout {
             case MotionEvent.ACTION_MOVE: {
                 if (allowDragging) {
                     if (isVertical) {
-                        float move = event.getY() - startY;
-                        viewCounter.setTranslationY(move > 0 ? Math.min(maxDistance, move) : Math.max(-maxDistance, move));
                         updateBtnBg(event);
                     } else {
                         viewCounter.setTranslationX(event.getX() - startX);
@@ -151,6 +134,7 @@ public class StepperTouchView extends ConstraintLayout {
             }
             case MotionEvent.ACTION_UP: {
                 allowDragging = false;
+                showWaterAnimation(event);
                 if (isVertical) {
                     if (viewCounter.getTranslationY() > viewCounter.getHeight() * 0.5 && allowPositive) {
                         //下
@@ -178,27 +162,7 @@ public class StepperTouchView extends ConstraintLayout {
                 }
                 if (isVertical) {
                     if (viewCounter.getTranslationY() != 0f) {
-                        SpringAnimation animY = new SpringAnimation(viewCounter, SpringAnimation.TRANSLATION_Y, 0f);
-                        SpringForce springForce = animY.getSpring();
-                        springForce.setStiffness(stiffness);
-                        springForce.setDampingRatio(damping);
-                        animY.setSpring(springForce);
-                        animY.addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
-                            @Override
-                            public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
-                                Log.e("lake", "onAnimationUpdate: " + (1f - value / maxDistance));
-                                float alpha = 1f - value / maxDistance;
-                                viewBtnBg.setAlpha(alpha);
-                                viewCounterIcon.setAlpha(alpha);
-                            }
-                        });
-                        animY.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
-                            @Override
-                            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
-                                viewBackground.setBackgroundResource(R.drawable.round_circle_bg);
-                            }
-                        });
-                        animY.start();
+
                     }
                 } else {
                     if (viewCounter.getTranslationX() != 0f) {
@@ -225,7 +189,12 @@ public class StepperTouchView extends ConstraintLayout {
      * @param event
      */
     private void updateBtnBg(MotionEvent event) {
-        float move = Math.abs(event.getY() - startY);
+        float disY = event.getY() - startY;
+        if (isSingle && disY > 0) {
+            return;
+        }
+        viewCounter.setTranslationY(disY > 0 ? Math.min(maxDistance, disY) : Math.max(-maxDistance, disY));
+        float move = Math.abs(disY);
         if (move >= maxDistance) {
             viewBtnBg.setAlpha(0f);
             viewCounterIcon.setAlpha(0f);
@@ -234,6 +203,99 @@ public class StepperTouchView extends ConstraintLayout {
             viewCounterIcon.setAlpha(1f - move / maxDistance);
         }
         viewBackground.setBackgroundResource(R.drawable.round_circle_bg_selector);
+        if (disY != 0) {
+            allowNegative(disY < 0);
+            allowPositive(disY > 0);
+        } else {
+            allowNegative(true);
+            allowPositive(true);
+        }
+    }
+
+    private void showWaterAnimation(MotionEvent event) {
+        if (viewCounter.getTranslationY() == 0f) {
+            return;
+        }
+        float disY = event.getY() - startY;
+        if (isSingle && disY > 0) {
+            return;
+        }
+        float move = Math.abs(disY);
+        ViewParent parent = getParent();
+        if (move >= (maxDistance - viewCounter.getWidth() / 2)) {//自吸到端位置
+            viewCounter.setTranslationY(disY > 0 ? maxDistance : -maxDistance);
+            viewBtnBg.setAlpha(0f);
+            viewCounterIcon.setAlpha(0f);
+            if (parent instanceof WaterViewLayout) {
+                final WaterViewLayout waterViewLayout = (WaterViewLayout) parent;
+                if (disY > 0) {
+                    waterViewLayout.showBottomWaterAnimation(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            showSpringAnimation();
+                            waterViewLayout.hideBottomWaterAnimation();
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                } else {
+                    waterViewLayout.showTopWaterAnimation(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            showSpringAnimation();
+                            waterViewLayout.hideTopWaterAnimation();
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                }
+
+            }
+        } else {
+            showSpringAnimation();
+        }
+    }
+
+    private void showSpringAnimation() {
+        SpringAnimation animY = new SpringAnimation(viewCounter, SpringAnimation.TRANSLATION_Y, 0f);
+        SpringForce springForce = animY.getSpring();
+        springForce.setStiffness(stiffness);
+        springForce.setDampingRatio(isSingle ? 1 : damping);
+        animY.setSpring(springForce);
+        animY.addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
+            @Override
+            public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
+                Log.e("lake", "onAnimationUpdate: " + (1f - value / maxDistance));
+                float alpha = 1f - value / maxDistance;
+                viewBtnBg.setAlpha(alpha);
+                viewCounterIcon.setAlpha(alpha);
+            }
+        });
+        animY.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                viewBackground.setBackgroundResource(R.drawable.round_circle_bg);
+                allowNegative(true);
+                allowPositive(true);
+            }
+        });
+        animY.start();
     }
 
     /**
@@ -263,7 +325,12 @@ public class StepperTouchView extends ConstraintLayout {
      */
     private void updateSideControls() {
         viewNegative.setVisibility(allowNegative ? VISIBLE : GONE);
-        viewPositive.setVisibility(allowPositive ? VISIBLE : GONE);
+        if (viewPositive != null)
+            viewPositive.setVisibility(allowPositive ? VISIBLE : GONE);
+
+        textNegative.setVisibility(allowNegative ? VISIBLE : GONE);
+        if (textPositive != null)
+            textPositive.setVisibility(allowPositive ? VISIBLE : GONE);
     }
 
     private boolean isInBounds(MotionEvent event, View view) {
